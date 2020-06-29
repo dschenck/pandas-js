a9cf4b8d5efc85f7b6f0f8cb8403bcf3519ad1c9
David Schenck <david.schenck@outlook.com>
2020-06-29 16:54:07 +0100

    added stats.min and stats.max

---
+++ src/core/stats/max.js
@@ -0,0 +1,5 @@
+import * as utils from '../utils'
+
+export default function min(values){
+    return values.reduce((prev, curr) => utils.isNaN(curr) ? prev : utils.isNaN(prev) ? curr : curr > prev ? curr : prev, NaN)
+}
